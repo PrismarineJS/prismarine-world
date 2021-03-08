46e21b6cb0183d8368f8243002d3b8a218c4d23c
Alexander Ivanov <47699394+Saiv46@users.noreply.github.com>
2021-03-08 22:00:17 +0800

    Update world.js

--- src/world.js
+++ src/world.js
@@ -121,8 +121,13 @@ class World extends EventEmitter {
     this.setLoadedColumn(chunkX, chunkZ, chunk, save)
   }
 
-  unloadColumn (chunkX, chunkZ) {
+  unloadColumn (chunkX, chunkZ, force = false) {
     const key = columnKeyXZ(chunkX, chunkZ)
+    if (this.savingQueue.has(key)) {
+      this.savingQueue.delete(key)
+      this.finishedSaving = Promise.all([this.finishedSaving,
+        this.storageProvider.save(chunkX, chunkZ, this.columns[key])])
+    }
     delete this.columns[key]
     const columnCorner = new Vec3(chunkX * 16, 0, chunkZ * 16)
     this.emit('chunkColumnUnload', columnCorner)
