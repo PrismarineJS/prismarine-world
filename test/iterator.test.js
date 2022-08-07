cf66897b84747ff0873828aee044f347cadda9c1
IceTank <61137113+IceTank@users.noreply.github.com>
2022-08-07 16:38:25 +0200

    Fix ManhattanIterator (#101)
    * Fix ManhattanIterator
    
    * Apply linting
    
    * Add basic ManhattanIterator iterator test

--- test/iterator.test.js
+++ test/iterator.test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 
-const { SpiralIterator2d } = require('../src/iterators')
+const { SpiralIterator2d, ManhattanIterator } = require('../src/iterators')
 const { Vec3 } = require('vec3')
 const expect = require('expect').default
 
@@ -13,5 +13,27 @@ describe('Spiral iterator', () => {
 
     expect(first.x === startPos.x && first.y === startPos.y && first.z === startPos.z).toBeTruthy()
     expect(second.x === startPos.x && second.y === startPos.y && second.z === startPos.z).toBeFalsy()
+  })
+})
+
+describe('ManhattanIterator iterator', () => {
+  it('First position is same as start', () => {
+    const start = new Vec3(1, 2, 3)
+    const iter = new ManhattanIterator(start.x, start.z, 5)
+    const first = iter.next()
+    expect(first.x === start.x && first.z === start.z).toBeTruthy()
+  })
+
+  it('Sample positions match', () => {
+    const start = new Vec3(1, 2, 3)
+    const iter = new ManhattanIterator(start.x, start.z, 5)
+    const sample = [new Vec3(1, 0, 3), new Vec3(2, 0, 3), new Vec3(1, 0, 4), new Vec3(0, 0, 3)]
+    let counter = 0
+    let next = iter.next()
+    while (next && counter < sample.length) {
+      expect(next.x === sample[counter].x && next.z === sample[counter].z).toBeTruthy()
+      next = iter.next()
+      counter++
+    }
   })
 })
