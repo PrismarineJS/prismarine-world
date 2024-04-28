e46d026985b28f4c3f4cfe825ba15e743958e026
Elliott Friedrich <85990359+Ell1ott@users.noreply.github.com>
2024-04-28 22:58:45 +0200

    Add missing RaycastResult Import (#142)
    * Add missing RaycastResult Import
    
    * Change return type to use RaycastResult
    
    * Remove unused RaycastBlock import

--- types/world.d.ts
+++ types/world.d.ts
@@ -2,7 +2,7 @@ import type { EventEmitter } from "events";
 import type { Vec3 } from "vec3";
 import type { Block } from "prismarine-block";
 import loaderOfChunk from "prismarine-chunk";
-import type { RaycastBlock } from "./iterators";
+import type { RaycastResult } from "./iterators";
 
 export type Chunk = InstanceType<ReturnType<typeof loaderOfChunk>>;
 
@@ -56,7 +56,7 @@ export declare class World extends EventEmitter {
         direction: Vec3,
         range: number,
         matcher?: (block: Block) => boolean,
-    ): Promise<RaycastBlock | null>;
+    ): Promise<RaycastResult | null>;
 
     public getLoadedColumn(chunkX: number, chunkZ: number): Chunk;
 
